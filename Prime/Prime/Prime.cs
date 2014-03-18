0538920bb7bf7707904d5bfca0eaf63e21ef25a7
singha4 <amandeep159632478@gmail.com>
2014-03-18 09:38:42 -0400

    version 1

---
+++ Prime/Prime/Prime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 3; i <= 150; i += 2)
+            {
+
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0)
+            {
+                
+                Console.WriteLine(i);
+                
+            }
+
+        }
+            }
+    }
+}
